817a5c708f087c2bfb0c4146994a3459cca2e076
Sergiu Barbus <danvbe@gmail.com>
2019-05-09 17:47:16 +0300

    testing

--- tests/Controller/QuoteAPITest.php
+++ tests/Controller/QuoteAPITest.php
@@ -18,20 +18,12 @@ class QuoteAPITest extends JsonApiTestCase{
 
 	public function testGetRandomQuoteResponse()
 	{
-		$this->loadFixturesFromFile('quotes.yaml');
-
 		$app_id = $this::APP_ID;
-		$this->client->request('GET', "http://127.0.0.1:8000/api/quote/random/$app_id");
+		$this->client->request('GET', "/api/quote/random/$app_id");
 
 		$response = $this->client->getResponse();
 
 		$this->assertResponse($response, 'single_quote_generic_response');
 	}
-
-	/*
-	public function testGetQuotesResponse(){
-		$this->loadFixturesFromFile('quotes.yaml');
-	}
-	*/
 
 }
--- tests/Controller/QuoteTest.php
+++ tests/Controller/QuoteTest.php
@@ -41,8 +41,89 @@ class QuoteTest extends WebTestCase {
 	{
 		$app_id = static::APP_ID;
 		return [
-			["http://127.0.0.1:8000/api/quote/random/$app_id"],    //random quote
-			["http://127.0.0.1:8000/api/quote/$app_id/authors"],   //all authors
+			["/api/quote/random/$app_id"],    //random quote
+			["/api/quote/$app_id/authors"],   //all authors
 		];
+	}
+
+	public function testPostUpdateDeleteRoutes(){
+		$client = self::createClient();
+		$app_id = static::APP_ID;
+		$url = "/api/quote/$app_id";
+		$data = array(
+			'author' => 'Anonymous',
+			'text' => 'This is a dummy quote',
+			'appId' => $app_id,
+		);
+
+		$client->request(
+			'POST',
+			$url,
+			$data,
+			[],
+			[
+				'CONTENT_TYPE' => 'application/json',
+			]
+		);
+
+		//we have it working
+		$this->assertTrue($client->getResponse()->isSuccessful());
+		//we get 201 code - HTTP CREATED
+		$this->assertEquals(201, $client->getResponse()->getStatusCode());
+
+		$response_data = json_decode($client->getResponse()->getContent(),true);
+
+		//we get the saved data
+		$this->assertTrue(($data['author'] == $response_data['author'])
+		                  && ($data['appId'] == $response_data['appId']) &&
+		                  ($data['text'] == $response_data['text']));
+
+		$id = $response_data['id'];
+
+		//LET's UPDATE the entity
+		$data = array(
+			'author' => 'Anonymous',
+			'text' => 'This is a dummy quote - updated',
+			'appId' => $app_id,
+		);
+		$url = "/api/quote/$app_id/$id";
+		$client->request(
+			'PUT',
+			$url,
+			$data,
+			[],
+			[
+				'CONTENT_TYPE' => 'application/json',
+			]
+		);
+
+		//we have it working
+		$this->assertTrue($client->getResponse()->isSuccessful());
+		//we get 200 code - HTTP OK
+		$this->assertEquals(200, $client->getResponse()->getStatusCode());
+
+		$response_data = json_decode($client->getResponse()->getContent(),true);
+
+		//we get the saved data
+		$this->assertTrue(($data['author'] == $response_data['author'])
+		                  && ($data['appId'] == $response_data['appId']) &&
+		                  ($data['text'] == $response_data['text']));
+
+		//LET's DELETE the entity
+		$url = "/api/quote/$app_id/$id";
+		$client->request(
+			'DELETE',
+			$url,
+			[],
+			[],
+			[
+				'CONTENT_TYPE' => 'application/json',
+			]
+		);
+
+		//we have it working
+		$this->assertTrue($client->getResponse()->isSuccessful());
+		//we get 204 code - HTTP NO CONTENT
+		$this->assertEquals(204, $client->getResponse()->getStatusCode());
 	}
 }
